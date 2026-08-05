a7b0a3517d6e20fe54ff44c21d59e1a8d2727438
yjhss <maprineto@gmail.com>
2026-08-05 15:48:40 +0900

    feat(schedule): 여행일정 상세 조회 API 엔드포인트 추가 (#72)

--- backend/src/main/java/com/tripass/schedule/controller/ScheduleController.java
+++ backend/src/main/java/com/tripass/schedule/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package com.tripass.schedule.controller;
 
 import com.tripass.common.response.ApiResponse;
+import com.tripass.schedule.dto.ScheduleDetailResponseDto;
 import com.tripass.schedule.dto.ScheduleListResponseDto;
 import com.tripass.schedule.service.ScheduleService;
 import io.swagger.annotations.Api;
@@ -8,11 +9,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @Api(tags = "여행 일정")
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/trips/{tripId}/schedules")
@@ -27,11 +31,35 @@ public class ScheduleController {
     @GetMapping
     public ResponseEntity<ApiResponse<List<ScheduleListResponseDto>>> getSchedules(
             @ApiParam(value = "여행 ID", required = true, example = "1")
-            @PathVariable Long tripId
+            @PathVariable
+            @Positive(message = "여행 ID는 양수여야 합니다.")
+            Long tripId
     ) {
 
         List<ScheduleListResponseDto> data = scheduleService.getSchedules(tripId);
 
         return ResponseEntity.ok(ApiResponse.success("여행 일정 목록 조회 성공", data));
+    }
+
+
+    @ApiOperation(
+            value = "여행 일정 상세 조회",
+            notes = "선택한 여행에 포함된 일정의 상세 정보를 조회합니다."
+    )
+    @GetMapping("/{scheduleId}")
+    public ResponseEntity<ApiResponse<ScheduleDetailResponseDto>> getScheduleDetail(
+            @ApiParam(value = "여행 ID", required = true, example = "1")
+            @PathVariable
+            @Positive(message = "여행 ID는 양수여야 합니다.")
+            Long tripId,
+            @ApiParam(value = "여행 일정 ID", required = true, example = "1")
+            @PathVariable
+            @Positive(message = "여행 일정 ID는 양수여야 합니다.")
+            Long scheduleId
+    ) {
+        ScheduleDetailResponseDto data = scheduleService.getScheduleDetail(tripId, scheduleId);
+
+        return ResponseEntity.ok(ApiResponse.success("여행 일정 상세 조회 성공", data)
+        );
     }
 }
