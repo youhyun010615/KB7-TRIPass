d297f290e2e3bedd6fe43c39d0041ebd5f32fa47
권유현 (Kweon Yu Hyun) <133007143+youhyun010615@users.noreply.github.com>
2026-08-16 22:31:31 +0900

    test(common): 최신 연관 API 계약 회귀 테스트 반영 (#262)

--- backend/src/test/java/com/tripass/exchange/ExchangeApiConnectionTest.java
+++ backend/src/test/java/com/tripass/exchange/ExchangeApiConnectionTest.java
@@ -14,11 +14,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.List;
 
 @Slf4j
 @ExtendWith(SpringExtension.class)
+@WebAppConfiguration
 @ContextConfiguration(classes = RootConfig.class)
 public class ExchangeApiConnectionTest {
 
